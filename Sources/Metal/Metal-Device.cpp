cd664814c63ba1c23994e6f60ac886748b393f86
piking77420 <bon.gabriel77@gmail.com>
2026-09-13 18:21:34 +0200

    Add metal backend (TODO test on mac os)

---
+++ Sources/Metal/Metal-Device.cpp
@@ -0,0 +1,27 @@
+#ifndef TITANIUM_METAL_DEVICE_H
+#define TITANIUM_METAL_DEVICE_H
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+
+#include <Metal/Metal.hpp>
+#include <MetalFX/MetalFX.hpp>
+
+#include <Metal/Metal.hpp>
+
+#include <Titanium/Device.hpp>
+
+namespace TiRHI
+{
+
+    Device::Device()
+    {
+    }
+
+    Device::~Device()
+    {
+    }
+}
+
+#endif // TITANIUM_METAL_DEVICE_H
