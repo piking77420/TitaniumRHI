b715a0411b3d63e020cb7889f1028b1fef5b1d82
piking77420 <bon.gabriel77@gmail.com>
2026-09-13 18:58:24 +0200

    Add macro for validationLayer

---
+++ Sources/Titanium/TitaniumHeader.hpp
@@ -0,0 +1,13 @@
+#ifndef TITANIUM_TITIANUIM_HEADER_H
+#define TITANIUM_TITIANUIM_HEADER_H
+
+namespace TiRHI
+{
+#if defined(TITANIUM_VALIDATION_LAYER)
+    static constexpr bool ValidationLayer = true;
+#else
+    static constexpr bool ValidationLayer = true;
+#endif
+}
+
+#endif // TITANIUM_TITIANUIM_HEADER_H
